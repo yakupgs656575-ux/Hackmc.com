83fee08135b93374b687804e6b1fcd174ed9a880
yakupgs656575-ux <yakupgs656575@gmail.com>
2026-07-10 17:53:01 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function toggleMenu() {
+    var menu = document.getElementById("sideMenu");
+    if (menu.style.width === "250px") {
+        menu.style.width = "0";
+    } else {
+        menu.style.width = "250px";
+    }
+}
+
